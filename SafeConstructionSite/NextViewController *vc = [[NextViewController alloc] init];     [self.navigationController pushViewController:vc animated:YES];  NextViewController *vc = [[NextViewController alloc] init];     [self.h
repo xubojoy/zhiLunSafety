b0aa21b61319e3d1118a0026d5b7777a9157cb0e
xubojoy <xubojoy1112@163.com>
2014-11-14 14:17:29 +0800

    优化

---
+++ SafeConstructionSite/NextViewController *vc = [[NextViewController alloc] init];     [self.navigationController pushViewController:vc animated:YES];  NextViewController *vc = [[NextViewController alloc] init];     [self.h
@@ -0,0 +1,13 @@
+//
+//  NextViewController *vc = [[NextViewController alloc] init];     [self.navigationController pushViewController:vc animated:YES];  NextViewController *vc = [[NextViewController alloc] init];     [self.h
+//  SafeConstructionSite
+//
+//  Created by myStyle on 14-11-10.
+//  Copyright (c) 2014年 XB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NextViewController__vc_____NextViewController_alloc__init________self_navigationController_pushViewController_vc_animated_YES____NextViewController__vc_____NextViewController_alloc__init________self : UIViewController
+
+@end
